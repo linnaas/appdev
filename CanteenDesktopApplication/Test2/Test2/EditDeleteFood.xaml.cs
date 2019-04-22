755529ee09c2b041163e4b96fe0e190d307b352f
Lizeth Guevara Capiral <lizethguevara@live.no>
2019-04-22 09:42:05 +0200

    SQL string Database

--- CanteenDesktopApplication/Test2/Test2/EditDeleteFood.xaml.cs
+++ CanteenDesktopApplication/Test2/Test2/EditDeleteFood.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Test2
 {
@@ -24,5 +26,56 @@ namespace Test2
         {
             InitializeComponent();
         }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection("server = 178.62.232.207; port = 3306; database = gruppe4; user = gruppe4; password = apputvikling;"))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select * from food", conn);
+
+                using (var reader = cmd.ExecuteReader())
+
+                {
+
+                    while (reader.Read())
+
+                    {
+
+                        int foodid = reader.GetInt32("food_id");
+
+                        string foodname = reader.GetString("food_name");
+
+                        int enabled = reader.GetInt32("enabled");
+
+                        int price = reader.GetInt32("price");
+
+                        int imageid = reader.IsDBNull(4) ? 0 : reader.GetInt32("image_id");
+
+                        System.Diagnostics.Debug.WriteLine("Image id: " + imageid);
+
+                        string description = reader.GetString("description");
+
+
+                        if (enabled == 1)
+
+                        {
+
+                            list.Add(new Food(foodid, foodname, enabled, price, imageid, description));
+
+                        }
+
+                    }
+
+                }
+
+                conn.Close();
+
+            }
+
+            return list;
+
+        }
+    }
     }
 }
